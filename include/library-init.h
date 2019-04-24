6e2e5f01a313a473f6e23e00d7f175f9c0a5efc5
Frank Stein <dr.frank.stain@gmail.com>
2019-04-24 14:37:33 +0300

    First implementation for top-level header.

---
+++ include/library-init.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+// General dependencies.
+#include <black.core.h>
+
+
+// Namespace initialization.
+namespace Black
+{
+// DOD (Data Oriented Design) library.
+inline namespace DataOriented
+{
+
+}
+}
