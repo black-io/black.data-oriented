51d50e08c9f8b409e86970aae8b1b6724996e232
Frank Stein <dr.frank.stain@gmail.com>
2019-04-25 08:58:09 +0300

    Master-header for composition subsystem.

---
+++ include/black.composition.h
@@ -0,0 +1,37 @@
+#pragma once
+
+
+// Library initialization header.
+#include "init.h"
+
+
+namespace Black
+{
+inline namespace DataOriented
+{
+// `Composition over inheritance` subsystem.
+inline namespace Composition
+{
+// Internal definitions for composition.
+namespace Internal
+{
+
+}
+}
+}
+}
+
+
+// Most core definitions.
+#include "composition/intrenal/internal.struct.h"
+
+// Size extraction adapter
+// Alignment extraction adapter
+// Composition length (int components) adapter.
+
+#include "composition/intrenal/MemoryBlock.h"
+// MemoryStructuredBlock
+// Basic component (optional inheritance)
+
+// Composition container.
+// Composition
