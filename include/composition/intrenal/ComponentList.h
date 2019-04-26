d2756705812328042094c750d338bebcfa4b3c12
Frank Stein <dr.frank.stain@gmail.com>
2019-04-26 23:09:37 +0300

    Changing the doc style.

--- include/composition/intrenal/ComponentList.h
+++ include/composition/intrenal/ComponentList.h
@@ -9,26 +9,31 @@ inline namespace Composition
 {
 namespace Internal
 {
-	/// @brief	Associate the list of components with indices.
+	// Associate the list of components with indices.
 	template< typename... TComponents >
-	class ComponentList final
+	struct ComponentList
 	{
-	// Public inner types and constants.
-	public:
-		/// @brief	Trivial list of components.
-		using Components = typename ComponentCollection<TComponents...>;
+		// Trivial list of components.
+		using Components = typename TypesCollection<TComponents...>;
 
-		/// @brief	Length of component list.
+
+		// Length of component list.
 		static constexpr const size_t LENGTH = sizeof...( TComponents );
 
-	// Public static interface.
-	public:
-		/// @brief	Get the length of component list.
+
+		// Get the length of component list.
 		static constexpr const size_t GetLength()	{ return LENGTH; };
 
-		/// @brief	Get the index of component. `Black::UNDEFINED_INDEX` will be returned if `TComponent` not in list of components.
+		// Get the index of component. `Black::UNDEFINED_INDEX` will be returned if `TComponent` not in list of components.
 		template< typename TComponent >
 		static constexpr const size_t GetIndex()	{ return typename ComponentIndexHelper<TComponent, Components, 0>::Find(); };
+	};
+
+	// Deduction branch to simplify the type construction from component collection.
+	template< typename... TComponents >
+	struct ComponentList<TypesCollection<TComponents...>> : public ComponentList<TComponents...>
+	{
+
 	};
 }
 }
