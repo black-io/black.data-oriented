d459aedba7dfeac6f985c353f8c19527f2e6d648
Frank Stein <dr.frank.stain@gmail.com>
2019-04-26 12:29:07 +0300

    Useful types to get index of type in parameter pack.

--- include/composition/intrenal/internal.struct.h
+++ include/composition/intrenal/internal.struct.h
@@ -22,6 +22,43 @@ namespace Internal
 		/// @brief	Specified internal offset of chunk.
 		static constexpr const size_t OFFSET	= CHUNK_OFFSET;
 	};
+
+	/// @brief	Trivial list of components.
+	template< typename... TComponents >
+	struct ComponentCollection final
+	{
+		static constexpr const size_t LENGTH = sizeof...( TComponents );
+	};
+
+	/// @brief	Component indexing helper. Used to associate the component type with ordinal index in list.
+	template< typename TComponent, typename TList, size_t BASE_INDEX >
+	struct ComponentIndexHelper final
+	{
+		static const size_t Find();
+	};
+
+	/// @brief	Terminal branch. The component is not found.
+	template< typename TComponent, size_t BASE_INDEX >
+	struct ComponentIndexHelper<TComponent, ComponentCollection<>, BASE_INDEX> final
+	{
+		static constexpr const size_t Find()	{ return Black::UNDEFINED_INDEX; };
+	};
+
+	/// @brief	Terminal branch. The component was found.
+	template< typename TComponent, size_t BASE_INDEX, typename... TRest >
+	struct ComponentIndexHelper<TComponent, ComponentCollection<TComponent, TRest...>, BASE_INDEX> final
+	{
+		static constexpr const size_t Find()	{ return BASE_INDEX; };
+	};
+
+	/// @brief	Deduction branch. Looking for the component in list.
+	template< typename TComponent, size_t BASE_INDEX, typename TCurrent, typename... TRest >
+	struct ComponentIndexHelper<TComponent, ComponentCollection<TCurrent, TRest...>, BASE_INDEX> final
+	{
+		using NextHelper = typename ComponentIndexHelper<TComponent, typename ComponentCollection<TRest...>, BASE_INDEX + 1>;
+
+		static constexpr const size_t Find()	{ return NextHelper::Find(); };
+	};
 }
 }
 }
