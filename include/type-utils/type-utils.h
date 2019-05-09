094f76c65d0dee61b48601720321884ac76ffb1b
Frank Stein <dr.frank.stain@gmail.com>
2019-05-09 10:47:30 +0300

    Master-header for type-utils.

---
+++ include/type-utils/type-utils.h
@@ -0,0 +1,29 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace DataOriented
+{
+// Type utilities.
+inline namespace TypeUtils
+{
+// Internal namespace for type utilities.
+namespace Internal
+{
+
+}
+}
+}
+}
+
+
+// Numeric collection.
+#include "internal/NumericCollection.h"
+#include "NumericCollection.h"
+
+// Type collections.
+#include "internal/TypesCollection.h"
+#include "internal/TypesUnion.h"
+#include "TypesCollection.h"
+#include "TypesUnion.h"
