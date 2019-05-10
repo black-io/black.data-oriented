323ff4aee3da593393bbb78851ab2b16e8eedf80
Frank Stein <dr.frank.stain@gmail.com>
2019-05-10 18:51:57 +0300

    New helpers for component mapping.

--- include/composition/intrenal/internal.struct.h
+++ include/composition/intrenal/internal.struct.h
@@ -103,6 +103,113 @@ namespace Internal
 		// Alignment of union.
 		static constexpr const size_t ALIGNMENT	= NumericCollection<TypeStats<TTypes>::ALIGNMENT...>::MAXIMUM;
 	};
+
+	// Helper to merge the numeric collections.
+	template< typename... TCollections >
+	struct NumericCollectionMergeHelper;
+
+	// Deduction branch. Keep merging the collections.
+	template< size_t... NUMBERS, typename... TRest >
+	struct NumericCollectionMergeHelper<NumericCollection<NUMBERS...>, TRest...>
+	{
+		// Result of merging.
+		using Collection = typename NumericCollection<NUMBERS...>::template MergeTailCollection<typename NumericCollectionMergeHelper<TRest...>::Collection>;
+	};
+
+	// Terminal branch. The last collection found.
+	template< size_t... NUMBERS >
+	struct NumericCollectionMergeHelper<NumericCollection<NUMBERS...>>
+	{
+		// Merged numbers.
+		using Collection = NumericCollection<NUMBERS...>;
+	};
+
+	// Component offset builder.
+	template< size_t BASE_OFFSET, typename... TTypes >
+	struct TypesCollectionOffsetBuilder;
+
+	// Deduction branch. Offset for regular type.
+	template< size_t BASE_OFFSET, typename THead, typename... TRest >
+	struct TypesCollectionOffsetBuilder<BASE_OFFSET, THead, TRest...> final
+	{
+		// Current offset information.
+		using OffsetInfo	= TypeOffset<THead, BASE_OFFSET>;
+
+		// Collection tail information.
+		using RestInfo		= TypesCollectionOffsetBuilder<OffsetInfo::NEXT_OFFSET, TRest...>;
+
+		// Collection offsets.
+		using Offsets		= typename NumericCollection<OffsetInfo::OFFSET>::template MergeTailCollection<typename RestInfo::Offsets>;
+	};
+
+	// Deduction branch. Offset for regular type.
+	template< size_t BASE_OFFSET, typename... TInnerTypes, typename... TRest >
+	struct TypesCollectionOffsetBuilder<BASE_OFFSET, Black::TypesCollection<TInnerTypes...>, TRest...> final
+	{
+		// Current offset information.
+		using OffsetInfo	= TypeOffset<Black::TypesCollection<TInnerTypes...>, BASE_OFFSET>;
+
+		// Collection tail information.
+		using RestInfo		= TypesCollectionOffsetBuilder<OffsetInfo::NEXT_OFFSET, TRest...>;
+
+		// Inner offsets.
+		using InnerOffsets	= typename TypesCollectionOffsetBuilder<BASE_OFFSET, Black::TypesCollection<TInnerTypes...>>::Offsets;
+
+		// Collection offsets.
+		using Offsets		= typename InnerOffsets::template MergeTailCollection<typename RestInfo::Offsets>;
+	};
+
+	// Deduction branch. Offset for regular type.
+	template< size_t BASE_OFFSET, typename... TInnerTypes, typename... TRest >
+	struct TypesCollectionOffsetBuilder<BASE_OFFSET, Black::TypesUnion<TInnerTypes...>, TRest...> final
+	{
+		// Current offset information.
+		using OffsetInfo	= TypeOffset<Black::TypesUnion<TInnerTypes...>, BASE_OFFSET>;
+
+		// Collection tail information.
+		using RestInfo		= TypesCollectionOffsetBuilder<OffsetInfo::NEXT_OFFSET, TRest...>;
+
+		// Inner offsets.
+		using InnerOffsets	= typename TypesCollectionOffsetBuilder<BASE_OFFSET, Black::TypesUnion<TInnerTypes...>>::Offsets;
+
+		// Collection offsets.
+		using Offsets		= typename InnerOffsets::template MergeTailCollection<typename RestInfo::Offsets>;
+	};
+
+	// Terminal branch. Offsets for last collection.
+	template< size_t BASE_OFFSET, typename... TInnerTypes >
+	struct TypesCollectionOffsetBuilder<BASE_OFFSET, Black::TypesCollection<TInnerTypes...>> final
+	{
+		// Information about offset of collection.
+		using OffsetInfo	= TypeOffset<Black::TypesCollection<TInnerTypes...>, BASE_OFFSET>;
+
+		// Collection offsets.
+		using Offsets		= typename TypesCollectionOffsetBuilder<OffsetInfo::OFFSET, TInnerTypes...>::Offsets;
+	};
+
+	// Terminal branch. Offsets for last union.
+	template< size_t BASE_OFFSET, typename... TInnerTypes >
+	struct TypesCollectionOffsetBuilder<BASE_OFFSET, Black::TypesUnion<TInnerTypes...>> final
+	{
+		// Information about offset of union.
+		using OffsetInfo	= TypeOffset<Black::TypesUnion<TInnerTypes...>, BASE_OFFSET>;
+
+		// Collection offsets.
+		using Offsets		= typename NumericCollectionMergeHelper<
+			typename TypesCollectionOffsetBuilder<OffsetInfo::OFFSET, TInnerTypes>::Offsets...
+		>::Collection;
+	};
+
+	// Terminal branch. The last type found.
+	template< size_t BASE_OFFSET, typename TLast >
+	struct TypesCollectionOffsetBuilder<BASE_OFFSET, TLast> final
+	{
+		// Information about offset of current type.
+		using OffsetInfo	= TypeOffset<TLast, BASE_OFFSET>;
+
+		// Collection offsets.
+		using Offsets		= typename NumericCollection<OffsetInfo::OFFSET>;
+	};
 }
 }
 }
