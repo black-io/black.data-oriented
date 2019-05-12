e39f0fe2ec71d8f62e71bfe686d1725d27da3121
Frank Stein <dr.frank.stain@gmail.com>
2019-05-12 14:25:16 +0300

    List of destructors for collection of composition parts.

---
+++ include/composition/intrenal/PartDestructorList.h
@@ -0,0 +1,30 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace DataOriented
+{
+inline namespace Composition
+{
+namespace Internal
+{
+	// List of destruction functions for collection of composition parts.
+	template< typename THost, typename... TParts >
+	struct PartDestructorList
+	{
+		static constexpr const Black::GlobalFunctionPointer<void, void*> FUNCTIONS[] = {
+			&PartConstructionPolicy<TParts, THost>::Destruct...
+		};
+	};
+
+	// Deduction branch. Simplify the parts deduction from collection.
+	template< typename THost, typename... TParts >
+	struct PartDestructorList<THost, Black::TypesCollection<TParts...>> : PartDestructorList<THost, TParts...>
+	{
+
+	};
+}
+}
+}
+}
