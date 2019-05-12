16456fc36845896f85eff1a6cbf30fa14521d891
Frank Stein <dr.frank.stain@gmail.com>
2019-05-12 15:26:00 +0300

    Allocation mask refactored.

--- include/composition/intrenal/AllocationMask.h
+++ include/composition/intrenal/AllocationMask.h
@@ -21,56 +21,56 @@ namespace Internal
 	// Public interface.
 	public:
 		// Mark the given index as allocated.
-		inline void MarkAllocated( const size_t index )				{ GetStorage( index ) = GetStorage( index ) | GetAllocationMask( index ); };
+		inline void MarkAllocated( const size_t index )				{ GetBuffer( index ) = GetBuffer( index ) | GetBitMask( index ); };
 
 		// Mark the given index as free.
-		inline void MarkFree( const size_t index )					{ GetStorage( index ) = GetStorage( index ) & ~GetAllocationMask( index ); };
+		inline void MarkFree( const size_t index )					{ GetBuffer( index ) = GetBuffer( index ) & ~GetBitMask( index ); };
 
 
 		// Whether the bit is up for given allocation index.
-		inline const bool IsAllocated( const size_t index ) const	{ return ( GetStorage( index ) & GetAllocationMask( index ) ) != 0; };
+		inline const bool IsAllocated( const size_t index ) const	{ return ( GetBuffer( index ) & GetBitMask( index ) ) != 0; };
 
 	// Private inner stuff.
 	private:
 		// Storage for bit-mask.
-		using StorageElement = size_t;
+		using BitBuffer = size_t;
 
 		// Storage element alignment in bits.
-		static constexpr const size_t ELEMENT_ALIGNMENT	= alignof( StorageElement ) * 8;
+		static constexpr const size_t BUFFER_BIT_ALIGNMENT	= alignof( BitBuffer ) * 8;
 
 		// Storage element size on bits.
-		static constexpr const size_t ELEMENT_SIZE		= sizeof( StorageElement ) * 8;
+		static constexpr const size_t BITS_PER_BUFFER		= sizeof( BitBuffer ) * 8;
 
 		// Aligned length of bit-mask storage.
-		static constexpr const size_t STORAGE_LENGTH	= Black::GetAlignedSize( MASK_LENGH, ELEMENT_ALIGNMENT ) / ELEMENT_SIZE;
+		static constexpr const size_t STORAGE_LENGTH		= Black::GetAlignedSize( MASK_LENGH, BUFFER_BIT_ALIGNMENT ) / BITS_PER_BUFFER;
 
 		// Single first bit of storage.
-		static constexpr const StorageElement FirstBit	= 1;
+		static constexpr const BitBuffer FirstBit			= 1;
 
 	// Private interface.
 	private:
 		// Get index of storage for given allocation index.
-		static constexpr const size_t GetStorageIndex( const size_t allocation_index )				{ return allocation_index / sizeof( StorageElement ); };
+		static constexpr const size_t GetBufferIndex( const size_t bit_index )	{ return bit_index / BITS_PER_BUFFER; };
 
 		// Get index of bit inside of storage for give allocation index.
-		static constexpr const size_t GetAllocationBit( const size_t allocation_index )				{ return allocation_index % sizeof( StorageElement ); };
+		static constexpr const size_t GetBitIndex( const size_t bit_index )		{ return bit_index % BITS_PER_BUFFER; };
 
 		// Get the bit-mask for given allocation index.
-		static constexpr const StorageElement GetAllocationMask( const size_t allocation_index )	{ return FirstBit << GetAllocationBit( allocation_index ); };
+		static constexpr const BitBuffer GetBitMask( const size_t bit_index )	{ return FirstBit << GetBitIndex( bit_index ); };
 
 		// Test the allocation index to be valid.
-		static inline void TestIndex( const size_t allocation_index )								{ EXPECTS_DEBUG( allocation_index < MASK_LENGH ); };
+		static inline void TestIndex( const size_t bit_index )					{ EXPECTS_DEBUG( bit_index < MASK_LENGH ); };
 
 
 		// Get bit storage for given allocation index.
-		inline StorageElement& GetStorage( const size_t allocation_index )							{ return m_bits[ GetStorageIndex( allocation_index ) ]; };
+		inline BitBuffer& GetBuffer( const size_t bit_index )					{ return m_bits[ GetBufferIndex( bit_index ) ]; };
 
 		// Get bit storage for given allocation index.
-		inline const StorageElement GetStorage( const size_t allocation_index ) const				{ return m_bits[ GetStorageIndex( allocation_index ) ]; };
+		inline const BitBuffer GetBuffer( const size_t bit_index ) const		{ return m_bits[ GetBufferIndex( bit_index ) ]; };
 
 	// Private state.
 	private:
-		StorageElement	m_bits[ STORAGE_LENGTH ];	// Allocation bits storage.
+		BitBuffer	m_bits[ STORAGE_LENGTH ];	// Allocation bits storage.
 	};
 }
 }
