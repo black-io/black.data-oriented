4a0c09bdd029bb7490e64fc6376012ba67183eb5
Frank Stein <dr.frank.stain@gmail.com>
2019-05-21 23:38:59 +0300

    Redundant definition for static constexpr array.

--- include/composition-over-inheritance/intrenal/PartDestructorList.h
+++ include/composition-over-inheritance/intrenal/PartDestructorList.h
@@ -13,10 +13,14 @@ namespace Internal
 	template< typename THost, typename... TParts >
 	struct PartDestructorList
 	{
-		static constexpr const Black::GlobalFunctionPointer<void, void*> FUNCTIONS[] = {
+		static constexpr const Black::GlobalFunctionPointer<void, void*> FUNCTIONS[ sizeof...( TParts ) ] = {
 			&PartConstructionPolicy<TParts, THost>::Destruct...
 		};
 	};
+
+	template< typename THost, typename... TParts >
+	constexpr const Black::GlobalFunctionPointer<void, void*> PartDestructorList<THost, TParts...>::FUNCTIONS[ sizeof...( TParts ) ];
+
 
 	// Deduction branch. Simplify the parts deduction from collection.
 	template< typename THost, typename... TParts >
