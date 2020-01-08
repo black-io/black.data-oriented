88e8c7fd21f55809cea5b8c772c6048743fa2fff
Frank Stein <dr.frank.stain@gmail.com>
2020-01-08 21:27:16 +0300

    Moving the initial header into core folder.

---
+++ include/core/init.h
@@ -0,0 +1,25 @@
+#pragma once
+
+
+// General dependencies.
+#include <black.core.h>
+
+
+// Namespace initialization.
+namespace Black
+{
+// DOD (Data Oriented Design) library.
+inline namespace DataOriented
+{
+// Internal namespace for DOD library.
+namespace Internal
+{
+
+}
+}
+}
+
+
+// Most core definitions.
+#include "../type-utils/type-utils.h"
+#include "../utils/utils.h"
