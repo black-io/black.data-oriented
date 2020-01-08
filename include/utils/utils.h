9ffafb5317ba67069219d2068b4f01ef855c1b7b
Frank Stein <dr.frank.stain@gmail.com>
2020-01-08 21:29:55 +0300

    Master-header for generic DOD utils.

---
+++ include/utils/utils.h
@@ -0,0 +1,22 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace DataOriented
+{
+// DOD utils subsystem.
+inline namespace Utils
+{
+// Internal namespace for DOD utils.
+namespace Internal
+{
+
+}
+}
+}
+}
+
+
+// Raw memory block.
+#include "RawMemoryBlock.h"
