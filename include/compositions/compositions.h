cc1ee56e08d7b4d5a06cc06f59b0f5713992af1a
Frank Stein <dr.frank.stain@gmail.com>
2021-01-02 23:47:18 +0300

    Composition header implemented.

--- include/compositions/compositions.h
+++ include/compositions/compositions.h
@@ -20,3 +20,22 @@ namespace Internal
 
 // Most core definitions.
 #include "internal/internal.struct.h"
+
+// Regular support for composition storage.
+#include "internal/AllocationMask.h"
+#include "internal/PartitionBlock.h"
+#include "internal/PartitionMediator.h"
+
+// Structured information about composition parts.
+#include "internal/TypesCollectionOffsetBuilder.h"
+#include "internal/PartitionMap.h"
+
+// Implementation of composition mediator.
+#include "HostRelatedComponent.h"
+#include "internal/PartConstructionPolicy.h"
+#include "internal/PartDestructorList.h"
+#include "Composition.h"
+
+
+// Deferred implementations.
+#include "Composition.inl"
