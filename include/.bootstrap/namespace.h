8b57a5d63e3fde64f6ebf829bd40d9c7b453530d
Frank Stein <dr.frank.stain@gmail.com>
2021-01-02 10:23:36 +0300

    Namespace declaration.

---
+++ include/.bootstrap/namespace.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+// Black Framework main namespace.
+namespace Black
+{
+// DOD (Data Oriented Design) library.
+inline namespace DataOriented
+{
+// Internal namespace for DOD library.
+namespace Internal
+{
+
+}
+}
+}
