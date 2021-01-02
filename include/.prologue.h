7ff0c762eb60cf12182bcb8ce0ba542a2419e7ad
Frank Stein <dr.frank.stain@gmail.com>
2021-01-02 18:48:24 +0300

    Prologue and epilogue completed.

---
+++ include/.prologue.h
@@ -0,0 +1,21 @@
+#pragma once
+
+
+// External dependencies.
+#include <black/core.h>
+
+
+// The library namespace definition.
+#include ".bootstrap/namespace.h"
+
+// Global forwards.
+#include ".global/types/forwards.h"
+
+// Global internal types.
+#include ".global/types/internal/TypesCollection.h"
+
+// Global public types.
+#include ".global/types/NumericCollection.h"
+#include ".global/types/TypesCollection.h"
+#include ".global/types/TypesUnion.h"
+#include ".global/types/RawMemoryBlock.h"
