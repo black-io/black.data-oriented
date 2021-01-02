18b6f241d697d2df7e14986ac58f708a58710c3d
Frank Stein <dr.frank.stain@gmail.com>
2021-01-02 18:10:40 +0300

    Forward declarations of required global types.

---
+++ include/.global/types/forwards.h
@@ -0,0 +1,22 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace DataOriented
+{
+inline namespace Global
+{
+inline namespace Types
+{
+	// Forward declaration for types collection.
+	template< typename... TTypes >
+	struct TypesCollection;
+
+	// Forward declaration for types union.
+	template< typename... TTypes >
+	struct TypesUnion;
+}
+}
+}
+}
