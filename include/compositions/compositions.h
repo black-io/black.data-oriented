8f877c093ec87d4d8093b5a0ccf5bd2c41c73c69
Frank Stein <dr.frank.stain@gmail.com>
2021-01-02 22:39:27 +0300

    First code for compositions subsystem header.

---
+++ include/compositions/compositions.h
@@ -0,0 +1,22 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace DataOriented
+{
+// Black Compositions subsystem.
+inline namespace Compositions
+{
+// Internal stuff for compositions.
+namespace Internal
+{
+
+}
+}
+}
+}
+
+
+// Most core definitions.
+#include "internal/internal.struct.h"
