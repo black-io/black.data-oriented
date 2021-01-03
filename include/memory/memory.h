dae5f571f771de15273fe62ad616ae53cbccc64e
Frank Stein <dr.frank.stain@gmail.com>
2021-01-03 10:17:46 +0300

    Basic stuff for header of subsystem.

---
+++ include/memory/memory.h
@@ -0,0 +1,29 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace DataOriented
+{
+// Memory management subsystem.
+inline namespace Memory
+{
+// Internal stuff for memory management.
+namespace Internal
+{
+
+}
+}
+}
+}
+
+
+// Fundamental internal stuff.
+#include "internal/forwards.h"
+#include "internal/interfaces.h"
+
+
+// Public implementations.
+
+
+// Deferred stuff.
